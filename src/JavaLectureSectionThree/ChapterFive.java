7831dd8ee30ce69c7d9a281470e54e65cb4afe22
Shaminder Singh <shaminder.qa@gmail.com>
2025-12-11 21:43:34 -0700

    Section3 Chapter 1-5

---
+++ src/JavaLectureSectionThree/ChapterFive.java
@@ -0,0 +1,48 @@
+package JavaLectureSectionThree;
+
+public class ChapterFive {
+
+   // Common Escape Sequences in Java (with Examples)
+
+
+    public static void main(String[] args) {
+
+        System.out.println("1");
+        System.out.println("I am learning java");
+
+        // - \n → moves to a new line.
+
+        System.out.println("2");
+        System.out.println("I am learning\n java");
+
+       // - \t → adds a tab space.
+
+        System.out.println("3");
+        System.out.println("I am learning\t java");
+
+        // - " → prints a double quote inside a string.
+
+        System.out.println("4");
+        System.out.println("I am learning\" java");
+
+      //  - ' → prints a single quote.
+
+        System.out.println("5");
+        System.out.println("I am learning\' java");
+
+        //- \ → prints a backslash.
+        System.out.println("6");
+        System.out.println("I am learning\\ java");
+
+       //- \r → carriage return (moves cursor to start of line).
+        System.out.println("7");
+        System.out.println("I am learning\r java");
+
+     // - \b → backspace (removes previous character).
+        System.out.println("8");
+        System.out.println("I am learning\b java");
+
+
+    }
+
+}
